82d653b82e0d3f0482c4b1e0db36205e2b30100c
220899CS08 <113495394+220899CS08@users.noreply.github.com>
2022-09-28 11:27:23 +0500

    Create javohiruz.cpp

---
+++ javohiruz.cpp
@@ -0,0 +1,96 @@
+//
+//  main.cpp
+//  Lab3work
+//
+//  Created by Javohirbek Khoshimov on 28/09/22.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    
+    /*
+    
+    problem 1
+    
+    int myNumber = 17;
+    cout << myNumber;
+    
+    float floatNumber = 4.3;
+    cout << floatNumber;
+    
+    double doubleNumber = 45.1234;
+    cout << doubleNumber;
+    
+    bool boolean = true;
+    cout << boolean;
+    
+    char charName = 'A';
+    cout << charName;
+    
+    */
+    
+    /*
+    
+    problem 2
+    
+    int number1(0);
+    int number2(0);
+    int sum(0);
+    
+    cout << "Enter the first number: ";
+    cin >> number1;
+    
+    cout << "Enter the second number: ";
+    cin >> number2;
+    
+    sum = number1 + number2;
+    
+    cout << "The sum is: " << sum << "\n";
+     
+    */
+
+    /*
+    
+    problem 3
+     
+    int a;
+    int b;
+    
+    cout << "Sum = " << a << " + " << b;
+    
+    */
+    
+    /*
+    
+    problem 4
+    
+    float number1(0);
+    float number2(0);
+    float sum(0);
+    
+    cout << "Enter first float number: ";
+    cin >> number1;
+    
+    cout << "Enter second float number: ";
+    cin >> number2;
+    
+    sum = number1 * number2;
+    
+    cout << "Product: " << sum << "\n";
+    
+    */
+    
+    bool zero = false;
+    bool one = true;
+    
+    cout << true;
+    cout << false;
+
+    cin.get ();
+    
+    return 0;
+}
